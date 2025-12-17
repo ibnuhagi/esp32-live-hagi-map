d91b8f1536fb3cca3cab09a2a7a72b8c22761401
ibnuhagi <ibnuhagi17@gmail.com>
2025-12-18 02:00:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Live Map ESP32</title>
+
+  <link rel="stylesheet" href="style.css">
+
+  <link 
+    rel="stylesheet"
+    href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+  />
+
+  <script 
+    src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js">
+  </script>
+
+  <script type="module" src="script.js"></script>
+</head>
+
+<body>
+  <div id="map"></div>
+</body>
+
+</html>
 
