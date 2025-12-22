ed4f94ef200d796d79b8978435462d436d910709
ibnuhagi <ibnuhagi17@gmail.com>
2025-12-23 01:22:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Konfigurasi Firebase Anda
 const firebaseConfig = {
     apiKey: "AIzaSyCaj0_uCRgLQyQ8GlP_lFarSEDUwmnnbMY",
     authDomain: "esp32-live-hagi.firebaseapp.com",
@@ -11,32 +10,37 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-
-// FIX: Path harus sesuai gambar Firebase Anda (device_01/live)
 const trackingRef = database.ref('device_01/live');
 
-// Marker motor (sesuaikan icon)
-const motorIcon = L.icon({
-    iconUrl: 'https://cdn-icons-png.flaticon.com/512/716/716429.png',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40]
+// SETUP MAP
+var mymap = L.map('map').setView([-6.1888, 106.2119], 15);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
+
+// PERBAIKAN LOGO: MENGGUNAKAN ICON MOTOR
+var motorIcon = L.icon({
+    iconUrl: 'https://cdn-icons-png.flaticon.com/512/716/716429.png', // Icon Motor
+    iconSize: [45, 45],
+    iconAnchor: [22, 45],
+    popupAnchor: [0, -40]
 });
 
-let marker = L.marker([-6.1888, 106.2119], {icon: motorIcon}).addTo(mymap);
+var marker = L.marker([-6.1888, 106.2119], {icon: motorIcon}).addTo(mymap);
 
-// REALTIME UPDATE TANPA REFRESH
-// Ganti bagian pembacaan data di script.js Anda
+// REALTIME UPDATE
 trackingRef.on('value', (snapshot) => {
     const data = snapshot.val();
     if (data) {
-        // Gunakan data.lon (huruf kecil semua) sesuai screenshot Firebase Anda
-        const newPos = new L.LatLng(data.lat, data.lon); 
+        // Sinkronisasi variabel: pastikan menggunakan data.lon (huruf kecil)
+        const newPos = new L.LatLng(data.lat, data.lon);
         marker.setLatLng(newPos);
         mymap.panTo(newPos);
         
+        // Update Panel Info
         document.getElementById('lat').innerText = data.lat;
-        document.getElementById('lng').innerText = data.lon; // Pastikan ini data.lon
-        document.getElementById('speed').innerText = data.speed;
+        document.getElementById('lng').innerText = data.lon;
+        document.getElementById('speed').innerText = data.speed + " km/jam";
         document.getElementById('timestamp').innerText = data.time;
+        
+        marker.bindPopup("<b>Motor Hagi</b><br>Kec: " + data.speed + " km/jam").openPopup();
     }
 });
